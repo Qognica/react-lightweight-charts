ad05502e55e03faaf9e2b132923519bfb96ff788
Simon Damberg <damberg.simon00@gmail.com>
2022-09-04 21:15:09 +0200

    idk anymore

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,42 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
+import dts from "rollup-plugin-dts";
 import external from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
 
-const packageJson = require('./package.json');
+const packageJson = require("./package.json");
 
 export default [
-    {
-        input: 'src/index.ts',
-        output: [
-            {
-                file: packageJson.main,
-                format: 'cjs',
-                sourcemap: true,
-                name: '@qognicafinance/react-lightweight-charts'
-            },
-            {
-                file: packageJson.module,
-                format: 'esm',
-                sourcemap: true
-            }
-        ],
-        plugins: [
-            external(),
-            resolve(),
-            commonjs(),
-            typescript({ tsconfig: './tsconfig.json' }),
-            postcss(),
-            terser()
-        ],
-    },
-]
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+        name: '@qognicafinance/react-lightweight-charts'
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+        external(),
+        resolve(),
+        commonjs(),
+        typescript({ tsconfig: './tsconfig.json' }),
+        postcss(),
+        terser()
+    ],
+  },
+  {
+    input: "dist/esm/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+    external: [/\.(css|less|scss)$/],
+  },
+];
--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,6 +1,95 @@
 import React, { FC, useEffect, useRef, useState } from 'react';
-import { createChart, IChartApi, OhlcData, SingleValueData } from 'lightweight-charts';
-import { IChart } from './Chart.types';
+import {
+    MouseEventHandler,
+    TimeRangeChangeEventHandler,
+    IChartApi,
+    SingleValueData,
+    CandlestickSeriesPartialOptions,
+    LineSeriesPartialOptions,
+    AreaSeriesPartialOptions,
+    BarSeriesPartialOptions,
+    HistogramSeriesPartialOptions,
+    OhlcData,
+    SeriesMarker,
+    ChartOptions,
+    PriceLineOptions,
+    createChart
+} from "lightweight-charts";
+
+
+export interface ICandlestickSeries {
+    id?: string;
+    data: OhlcData[];
+    options?: CandlestickSeriesPartialOptions;
+    linearInterpolation?: number;
+    legend?: string;
+    //markers?: SeriesMarker<>[]; TODO figure out TimeType thing
+    priceLines?: PriceLineOptions[];
+}
+
+export interface ILineSeries {
+    id?: string;
+    data: SingleValueData[];
+    options?: LineSeriesPartialOptions;
+    linearInterpolation?: number;
+    legend?: string;
+    //markers?: SeriesMarker[];
+    priceLines?: PriceLineOptions[];
+}
+
+export interface IAreaSeries {
+    id?: string;
+    data: SingleValueData[];
+    options?: AreaSeriesPartialOptions;
+    linearInterpolation?: number;
+    legend?: string;
+    //markers?: SeriesMarker[];
+    priceLines?: PriceLineOptions[];
+}
+
+export interface IBarSeries {
+    id?: string;
+    data: SingleValueData[];
+    options?: BarSeriesPartialOptions;
+    linearInterpolation?: number;
+    legend?: string;
+    //markers?: SeriesMarker[];
+    priceLines?: PriceLineOptions[];
+}
+
+export interface IHistogramSeries {
+    id?: string;
+    data: SingleValueData[];
+    options?: HistogramSeriesPartialOptions;
+    linearInterpolation?: number;
+    legend?: string;
+    //markers?: SeriesMarker[];
+    priceLines?: PriceLineOptions[];
+}
+
+type DeepPartial<T> = {
+    [P in keyof T]?: DeepPartial<T[P]>;
+};
+
+export interface IChart {
+    candlestickSeries?: Array<ICandlestickSeries>;
+    lineSeries?: Array<ILineSeries>;
+    areaSeries?: Array<IAreaSeries>;
+    barSeries?: Array<IBarSeries>;
+    histogramSeries?: Array<IHistogramSeries>;
+    width?: number;
+    height?: number;
+    options?: DeepPartial<ChartOptions>;
+    autoWidth?: boolean;
+    autoHeight?: boolean;
+    legend?: string;
+    from?: number;
+    to?: number;
+    onCrosshairMove?: MouseEventHandler;
+    onTimeRangeMove?: TimeRangeChangeEventHandler;
+    darkTheme?: boolean;
+    chartRef?: (chart: IChartApi) => void;
+}
 
 const Chart: FC<IChart> = (props) => {
     const { candlestickSeries, lineSeries, areaSeries, barSeries, histogramSeries, width, height, options, autoWidth, autoHeight, legend, from, to, onCrosshairMove, onTimeRangeMove, darkTheme, chartRef } = props;
